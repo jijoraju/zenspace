98b245289632ed69e2d912acc4e15dd87de8e178
FredLi <aikwdc00@hotmail.com>
2023-10-05 04:18:29 +0800

    sign UI bug

--- src/pages/Sign/index.jsx
+++ src/pages/Sign/index.jsx
@@ -1,4 +1,4 @@
-import React,{lazy} from 'react'
+import React,{lazy, useEffect,useState} from 'react'
 import { useLocation} from "react-router-dom";
 
 import Image from "@components/Images";
@@ -10,20 +10,29 @@ const Register = lazy(() => import('./register'));
 function Sign() {
   const location = useLocation()
   const {pathname}  = location
+  // const {imageName,setImageName} = useState(`signin`)
 
   const page = pathname == '/login'? <Login />:<Register />
   const imageName = pathname == '/login'? `signin`: `signup`
+
+  // useEffect(()=>{
+  //   function checkPath(){
+  //     const imageName = pathname == '/login'? `signin`: `signup`
+  //     setImageName(imageName)
+  //   }
+    
+  // },[pathname])
 
   return (
     <div className='sign-container'>
 
       <div className={`sign-container-area`}>
         <Image
-          src={`/${imageName}/${imageName}.png`}
+          src={`${imageName}/${imageName}.png`}
           alt={`${imageName}`}
           styles={`sign-container-area-image`}
-          img2={`/${imageName}/${imageName}@2x.png`}
-          img3={`/${imageName}/${imageName}@3x.png`}
+          img2={`${imageName}/${imageName}@2x.png`}
+          img3={`${imageName}/${imageName}@3x.png`}
         />
       </div>
 
