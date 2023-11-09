016dd4d1c34486f7d1f0b371f96becca2e0b185c
FredLi <aikwdc00@hotmail.com>
2023-11-09 15:07:17 -0500

    test GA

--- vite.config.js
+++ vite.config.js
@@ -23,5 +23,6 @@ export default defineConfig({
   define: {
     'MAP_API_KEY': JSON.stringify(process.env.VITE_MAP_API_KEY),
     'API_DOMAIN': JSON.stringify(process.env.VITE_API_DOMAIN),
+    // 'GA_KEY': JSON.stringify(process.env.GA_KEY),
   },
 })
